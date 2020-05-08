fb061f413da0bc095c0321eed633fb573d047bb9
Kausar Akther <kausar.akther@gmail.com>
2020-05-08 15:44:18 -0400

    Updates

---
+++ precache-manifest.ec1fed7b6ec0948752a9d610b2debc58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc3b86074d55660bb68fb318f28c373d",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "e9953dfb10a2bcc9b0d8",
+    "url": "/react-deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a1bc7dcb0ddda7c2efa0",
+    "url": "/react-deploy/static/js/2.d6d43cce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.d6d43cce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9953dfb10a2bcc9b0d8",
+    "url": "/react-deploy/static/js/main.85b9d983.chunk.js"
+  },
+  {
+    "revision": "4d275ecffff8f8311b08",
+    "url": "/react-deploy/static/js/runtime-main.8e4be4b4.js"
+  },
+  {
+    "revision": "2effa1f9e0b9b471c3bac08e25b154ac",
+    "url": "/react-deploy/static/media/portfolio_background.2effa1f9.jpg"
+  }
+]);
