00e3088812031cfdf01d3e6c7503e0f94a7a4b55
Kausar Akther <kausar.akther@gmail.com>
2020-05-08 15:42:46 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import profileImage from './img/portfolio_background.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Medical Termonology</h1>
+     <h2>Author: Dr. Aliza Islam and Dr. Tophika Islam </h2>
+     <img src={profileImage} alt="profile-img"  />
     </div>
   );
 }
